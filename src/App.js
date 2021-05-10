7b0d39c0245641e0d84789d7325f095dcf5f6b38
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-10 03:27:13 +0100

    some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Box, ThemeProvider, BaseStyles, Button } from '@primer/components';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider colorMode="night">
+      <BaseStyles>
+        <Box bg="bg.primary" height="100vh">
+          <Button>Hello</Button>
+        </Box>
+      </BaseStyles>
+    </ThemeProvider>
+  );
 }
 
 export default App;
