941890e548caaf48da7b0cc07bb287f320da0bcd
kali <moatezzchabane@gmail.com>
2021-05-10 23:57:01 +0100

    organized  Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { UnderlineNav, Box } from '@primer/components';
 
 function Nav() {
-  return <div></div>;
+  return (
+    <Box>
+      <UnderlineNav aria-label="Main">
+        <UnderlineNav.Link href="#home" selected>
+          Home
+        </UnderlineNav.Link>
+        <UnderlineNav.Link href="#documentation">
+          Documentation
+        </UnderlineNav.Link>
+        <UnderlineNav.Link href="#support">Support</UnderlineNav.Link>
+      </UnderlineNav>
+    </Box>
+  );
 }
 
 export default Nav;
