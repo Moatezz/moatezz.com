3b97ae37ccca06c71626fe7a7fe10c6d216df061
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-10 02:49:34 +0100

    init app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import logo from './logo.svg';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Coming soon...</p>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
