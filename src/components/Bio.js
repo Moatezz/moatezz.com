61c1548cc826aa647419fa31251e1b83d4b9f1ef
kali <moatezzchabane@gmail.com>
2021-05-11 21:44:36 +0100

    added Avatar

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { Box, Avatar } from '@primer/components';
+import WrapAvatar from './styles/WrapAvatar';
 function Bio() {
-  return <div></div>;
+  return (
+    <Box>
+      <WrapAvatar>
+        <Avatar
+          className="avatar"
+          src="https://avatars.githubusercontent.com/u/72275419?v=4"
+          size="250"
+        />
+      </WrapAvatar>
+    </Box>
+  );
 }
 
 export default Bio;
---
+++ src/components/styles/WrapAvatar.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  width: min-content;
+  border: 1px solid white;
+  border-radius: 50%;
+`;
