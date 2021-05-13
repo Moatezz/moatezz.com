e373d0445c7d11386d7c0475b79ecc530df8f258
kali <moatezzchabane@gmail.com>
2021-05-13 02:13:41 +0100

    good styling

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Box, Avatar, BorderBox, Text, Link } from '@primer/components';
+import {
+  Box,
+  Avatar,
+  BorderBox,
+  Text,
+  Link,
+  Flex,
+  StyledOcticon,
+} from '@primer/components';
 import { LocationIcon, MailIcon, ClippyIcon } from '@primer/octicons-react';
 import WrapAvatar from './styles/WrapAvatar';
 import Wrapper from './styles/Wrapper';
@@ -9,7 +17,7 @@ function Bio() {
   return (
     <Wrapper>
       <BorderBox ml={3} mt={3} p={3}>
-        <WrapAvatar>
+        <Flex flexDirection="column" alignItems="center">
           <BorderBox borderRadius={100}>
             <Avatar
               className="avatar"
@@ -17,35 +25,39 @@ function Bio() {
               size="200"
             />
           </BorderBox>
-        </WrapAvatar>
-        <Text lineHeight={0.6} as="h2">
-          Moatezz Chabane
-        </Text>
-        <Text my={2} lineHeight={0.2} as="p">
-          Full Stack Web Developer
-        </Text>
-        <Link lineHeight={0.1} href="https://www.moatezz.com">
-          www.moatezz.com
-        </Link>
-        <BorderBox>
-          <WrapPadding>
-            <FaLinkedin />
-            <FaGithub />
-            <FaWhatsapp />
-            <FaDiscord />
-          </WrapPadding>
+          <Box width="fit-content">
+            <Text lineHeight={0.6} as="h2">
+              Moatezz Chabane
+            </Text>
+          </Box>
+          <Box width="fit-content">
+            <Text my={2} lineHeight={0.2} as="p">
+              Full Stack Web Developer
+            </Text>
+          </Box>
+          <Link href="https://www.moatezz.com">www.moatezz.com</Link>
+        </Flex>
+        <BorderBox py={2} mt={3}>
+          <Flex flexDirection="row" justifyContent="space-around">
+            <FaLinkedin size={32} />
+            <FaGithub size={32} />
+            <FaWhatsapp size={32} />
+            <FaDiscord size={32} />
+          </Flex>
         </BorderBox>
-        <Box>
-          <LocationIcon size={16} />
-          <Text as="p">Algiers, Algeria</Text>
-        </Box>
-        <Box>
-          <MailIcon size={16} />
-          <Text as="p">moatezzchabane@gmail.com</Text>
-          <BorderBox>
-            <ClippyIcon aria-label="Copy Email address" size={16} />
-          </BorderBox>
-        </Box>
+        <Flex mt={3} flexDirection="row" alignItems="center">
+          <StyledOcticon icon={LocationIcon} size={24} pr={2} />
+          <Text my={0} as="p">
+            Algiers, Algeria
+          </Text>
+        </Flex>
+        <Flex my={1} flexDirection="row" alignItems="center">
+          <StyledOcticon icon={MailIcon} size={24} pr={2} />
+          <Text my={0} as="p" pr={2}>
+            moatezzchabane@gmail.com
+          </Text>
+          <StyledOcticon icon={ClippyIcon} size={16} />
+        </Flex>
       </BorderBox>
     </Wrapper>
   );
