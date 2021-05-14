70ec202af4e71833bb960178e9f2e16e4f83ec06
kali <moatezzchabane@gmail.com>
2021-05-14 01:26:13 +0100

    successfuly added navingation logic

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UnderlineNav, Box } from '@primer/components';
 
 function Nav() {
+  const [active, setActive] = useState({
+    projects: true,
+    packages: false,
+    wpPlugins: false,
+    resume: false,
+  });
+  const handleSelection = (target) => {
+    switch (target) {
+      case 'Projects':
+        setActive({
+          projects: true,
+          packages: false,
+          wpPlugins: false,
+          resume: false,
+        });
+
+        break;
+      case 'Packages':
+        setActive({
+          projects: false,
+          packages: true,
+          wpPlugins: false,
+          resume: false,
+        });
+        break;
+      case 'WP Plugins':
+        setActive({
+          projects: false,
+          packages: false,
+          wpPlugins: true,
+          resume: false,
+        });
+        break;
+      case 'Resume':
+        setActive({
+          projects: false,
+          packages: false,
+          wpPlugins: false,
+          resume: true,
+        });
+        break;
+      default:
+        setActive({
+          projects: true,
+          packages: false,
+          wpPlugins: false,
+          resume: false,
+        });
+        break;
+    }
+  };
+
   return (
     <Box>
       <UnderlineNav aria-label="Main" pl={400}>
-        <UnderlineNav.Link href="#home" selected>
+        <UnderlineNav.Link
+          href="#home"
+          selected={active.projects}
+          onClick={() => {
+            handleSelection('Projects');
+          }}
+        >
           Projects
         </UnderlineNav.Link>
-        <UnderlineNav.Link href="#documentation">Packages</UnderlineNav.Link>
-        <UnderlineNav.Link href="#support">WP Plugins</UnderlineNav.Link>
-        <UnderlineNav.Link href="#support">Resume</UnderlineNav.Link>
+        <UnderlineNav.Link
+          href="#documentation"
+          selected={active.packages}
+          onClick={() => {
+            handleSelection('Packages');
+          }}
+        >
+          Packages
+        </UnderlineNav.Link>
+        <UnderlineNav.Link
+          href="#support"
+          selected={active.wpPlugins}
+          onClick={() => {
+            handleSelection('WP Plugins');
+          }}
+        >
+          WP Plugins
+        </UnderlineNav.Link>
+        <UnderlineNav.Link
+          href="#support"
+          selected={active.resume}
+          onClick={() => {
+            handleSelection('Resume');
+          }}
+        >
+          Resume
+        </UnderlineNav.Link>
       </UnderlineNav>
     </Box>
   );
