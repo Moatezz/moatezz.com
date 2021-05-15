e4b024739e65ef9b99e86eadc43e948d32aec61b
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-15 22:43:00 +0100

    using grid

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import HeaderTop from './components/HeaderTop';
 import Bio from './components/Bio';
 import Main from './components/Main/Main';
 import Nav from './components/Nav';
-import { Box, ThemeProvider, BaseStyles } from '@primer/components';
+import { Box, ThemeProvider, BaseStyles, Grid } from '@primer/components';
 function App() {
   return (
     <ThemeProvider colorMode="night">
       <BaseStyles>
         <Box bg="bg.primary" minHeight="100vh">
           <HeaderTop />
-          <Nav />
-          <Bio />
-          <Main />
+          <Grid gridTemplateColumns="repeat(2,auto)">
+            <Bio />
+            <Box>
+              <Nav />
+              <Main />
+            </Box>
+          </Grid>
         </Box>
       </BaseStyles>
     </ThemeProvider>
