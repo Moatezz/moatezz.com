1c2c03c006f4aee2f28f72e91d9052c0c3f81df9
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-16 21:29:52 +0100

    going back

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import Screens from './components/Screens';
+import HeaderTop from './components/HeaderTop';
+import Bio from './components/Bio';
+import Main from './components/Main';
+import Nav from './components/Nav';
+import { Box, ThemeProvider, BaseStyles, Grid } from '@primer/components';
 function App() {
-  return <Screens />;
+  console.log(window.screen.orientation.type, window.screen.width);
+  const screen = () => {
+    let orientation = window.screen.orientation.type;
+    let width = window.screen.width;
+  };
+  return (
+    <ThemeProvider colorMode="night">
+      <BaseStyles>
+        <Box bg="bg.primary" minHeight="100vh" minWidth="100vw">
+          <HeaderTop />
+          <Nav />
+          <Grid gridTemplateColumns="1fr 4fr" mx={150}>
+            <Bio />
+            <Main />
+          </Grid>
+        </Box>
+      </BaseStyles>
+    </ThemeProvider>
+  );
 }
 
 export default App;
