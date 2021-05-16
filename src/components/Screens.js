ef47507a0664c02b50b74b03021e4c4c04a6d0b0
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-16 16:08:01 +0100

    added screen switcher

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -6,25 +6,66 @@ import Nav from './Nav';
 import { Box, ThemeProvider, BaseStyles, Grid } from '@primer/components';
 
 function Screens() {
-  console.log(window.screen.orientation.type, window.screen.width);
-  const screen = () => {
-    let orientation = window.screen.orientation.type;
-    let width = window.screen.width;
-  };
-  return (
+  const orientation = window.screen.orientation.type;
+  const width = window.screen.width;
+  const mobile = orientation === 'portrait-primary' && width < 480;
+  const desktop = orientation === 'landscape-primary' && width > 1200;
+  const tablet =
+    orientation === 'portrait-primary' && width < 800 && width > 480;
+  console.log('mobile: ' + mobile, 'tablet: ' + tablet, width);
+  if (desktop) {
+    return (
+      <ThemeProvider colorMode="night">
+        <BaseStyles>
+          <Box bg="bg.primary" minHeight="100vh" minWidth="100vw">
+            <HeaderTop />
+            <Nav />
+            <Grid gridTemplateColumns="1fr 4fr" mx={150}>
+              <Bio />
+              <Main />
+            </Grid>
+          </Box>
+        </BaseStyles>
+      </ThemeProvider>
+    );
+  } else if (mobile) {
+    return (
+      <ThemeProvider colorMode="night">
+        <BaseStyles>
+          <Box bg="bg.primary" minHeight="100vh" minWidth="100vw">
+            <HeaderTop />
+            <Nav />
+            <Grid gridTemplateColumns="1fr 4fr" mx={150}>
+              <Main />
+            </Grid>
+          </Box>
+        </BaseStyles>
+      </ThemeProvider>
+    );
+  } else if (tablet) {
+    return (
+      <ThemeProvider colorMode="night">
+        <BaseStyles>
+          <Box bg="bg.primary" minHeight="100vh" minWidth="100vw">
+            <HeaderTop />
+            <Nav />
+            <Grid gridTemplateColumns="1fr 4fr" mx={150}>
+              <Bio />
+            </Grid>
+          </Box>
+        </BaseStyles>
+      </ThemeProvider>
+    );
+  } else {
     <ThemeProvider colorMode="night">
       <BaseStyles>
         <Box bg="bg.primary" minHeight="100vh" minWidth="100vw">
           <HeaderTop />
           <Nav />
-          <Grid gridTemplateColumns="1fr 4fr" mx={150}>
-            <Bio />
-            <Main />
-          </Grid>
+          <Grid gridTemplateColumns="1fr 4fr" mx={150}></Grid>
         </Box>
       </BaseStyles>
-    </ThemeProvider>
-  );
+    </ThemeProvider>;
+  }
 }
-
 export default Screens;
