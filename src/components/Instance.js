55666c519bddafe755a463b31c3dae18d158fb7d
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-18 12:00:50 +0100

    so far so good

--- src/components/Instance.js
+++ src/components/Instance.js
@@ -1,18 +1,28 @@
-import { BorderBox, Link, Text } from '@primer/components';
+import { BorderBox, Flex, Link, Text } from '@primer/components';
 import React from 'react';
+import skillsIcons from './data/skillsIcons.json';
+import projects from './data/projects.json';
+import somethinging from './data/assets/react.svg';
 
 function Instance() {
+  const something = projects.something;
+  const iconsLink = (instance) => {
+    const targetedSkill = projects[instance].skills;
+    return skillsIcons[targetedSkill];
+  };
+  console.log(iconsLink('something'));
   return (
     <BorderBox m={3} p={3} fontSize={1}>
       <Link fontWeight={600}>Something</Link>
       <Text fontSize={1} as="p">
-        This is a description for a certain projectThis is a description for a
-        certain projectThis is a description for a certain projectThis is a
-        description
+        {something.description}
       </Text>
-      <Text fontSize={1} as="p">
-        React
-      </Text>
+      <Flex flexDirection="row" alignItems="center">
+        <img src={somethinging} height="25px" />
+        <Text fontSize={1} as="p">
+          React
+        </Text>
+      </Flex>
     </BorderBox>
   );
 }
