4c197c8beec011352b18c809fc831d0309c08b2f
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-18 18:25:30 +0100

    fixed it

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import Bio from './components/Bio';
 import Main from './components/Main';
 import Nav from './components/Nav';
 import { Box, ThemeProvider, BaseStyles, Grid } from '@primer/components';
-import { useDeviceContext } from './Context';
 import Mobile from './Mobile';
+import { useState, useEffect } from 'react';
 function App() {
-  const device = useDeviceContext();
-  console.log(device);
+  const [deviceState, setDeviceState] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resizeListener = () => {
+      console.log(window.innerWidth);
+      if (window.innerWidth <= 1024) {
+        return setDeviceState('mobile');
+      } else if (window.innerWidth >= 1024) {
+        return setDeviceState('desktop');
+      }
+    };
+    window.addEventListener('resize', resizeListener);
+
+    return () => {
+      window.removeEventListener('resize', resizeListener);
+    };
+  }, []);
+
   return (
     <Box>
-      {device === 'desktop' ? (
+      {deviceState === 'desktop' || deviceState >= 1024 ? (
         <ThemeProvider colorMode="night">
           <BaseStyles>
             <Box bg="bg.primary" minHeight="100vh" minWidth="100vw">
