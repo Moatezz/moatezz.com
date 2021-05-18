e81f388d6c4f82d78202a2479ad5787d6a3e7e0f
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-18 17:38:24 +0100

    good

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import Main from './components/Main';
 import Nav from './components/Nav';
 import { Box, ThemeProvider, BaseStyles, Grid } from '@primer/components';
 import { useDeviceContext } from './Context';
+import Mobile from './Mobile';
 function App() {
+  const device = useDeviceContext();
+  console.log(device);
   return (
-    <ThemeProvider colorMode="night">
-      <BaseStyles>
-        <Box bg="bg.primary" minHeight="100vh" minWidth="100vw">
-          <HeaderTop />
+    <Box>
+      {device === 'desktop' ? (
+        <ThemeProvider colorMode="night">
+          <BaseStyles>
+            <Box bg="bg.primary" minHeight="100vh" minWidth="100vw">
+              <HeaderTop />
 
-          <Nav />
+              <Nav />
 
-          <Grid gridTemplateColumns="1fr 4fr" mx={150}>
-            <Bio />
+              <Grid gridTemplateColumns="1fr 4fr" mx={150}>
+                <Bio />
 
-            <Main />
-          </Grid>
-        </Box>
-      </BaseStyles>
-    </ThemeProvider>
+                <Main />
+              </Grid>
+            </Box>
+          </BaseStyles>
+        </ThemeProvider>
+      ) : (
+        <Mobile />
+      )}
+    </Box>
   );
 }
 
