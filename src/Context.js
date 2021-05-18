d986e2e1e86aa894817df0d90c2da1ba3b4277b8
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-18 17:43:26 +0100

    finally everything working

--- src/Context.js
+++ src/Context.js
@@ -3,7 +3,7 @@ import React, { createContext, useState, useEffect, useContext } from 'react';
 export const DeviceContext = createContext();
 export const useDeviceContext = () => useContext(DeviceContext);
 function Context({ children }) {
-  const [deviceState, setDeviceState] = useState('desktop');
+  const [deviceState, setDeviceState] = useState('mobile');
   const getWidth = () =>
     window.innerWidth ||
     document.documentElement.clientWidth ||
@@ -11,8 +11,7 @@ function Context({ children }) {
 
   useEffect(() => {
     const resizeListener = () => {
-      if (getWidth() < 800 && getWidth() > 480) setDeviceState('tablet');
-      else if (getWidth() < 480) setDeviceState('mobile');
+      if (getWidth() <= 1024) setDeviceState('mobile');
       else setDeviceState('desktop');
     };
     window.addEventListener('resize', resizeListener);
@@ -21,7 +20,7 @@ function Context({ children }) {
       window.removeEventListener('resize', resizeListener);
     };
   }, []);
-  //console.log(deviceState);
+  console.log(getWidth());
 
   return (
     <DeviceContext.Provider value={deviceState}>
