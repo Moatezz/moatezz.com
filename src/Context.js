8fd15772c35fd4a687253365af9bf57eeebc7031
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-18 14:40:53 +0100

    maybe?

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,33 @@
-import React, { creatContext, useContext, useState } from 'react';
+import React, { creatContext, useState } from 'react';
 
 export const DeviceContext = creatContext();
 
 function Context({ children }) {
-  const [device, setDevice] = useState('desktop');
+  const [deviceState, setDeviceState] = useState('desktop');
+  const getWidth = () =>
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
 
-  return <DeviceContext.Provider>{children}</DeviceContext.Provider>;
+  useEffect(() => {
+    const resizeListener = () => {
+      if (getWidth() < 800) setDeviceState('tablet');
+      else if (getWidth() < 480) setDeviceState('mobile');
+      else setDeviceState('desktop');
+    };
+    window.addEventListener('resize', resizeListener);
+
+    return () => {
+      window.removeEventListener('resize', resizeListener);
+    };
+  }, []);
+  console.log(state);
+
+  return (
+    <DeviceContext.Provider value={deviceState}>
+      {children}
+    </DeviceContext.Provider>
+  );
 }
 
 export default Context;
