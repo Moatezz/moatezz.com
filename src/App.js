0eda9f5967741b4c1a3d7746f47dbae513446b80
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-18 15:54:03 +0100

    everything good so far

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import Bio from './components/Bio';
 import Main from './components/Main';
 import Nav from './components/Nav';
 import { Box, ThemeProvider, BaseStyles, Grid } from '@primer/components';
-import { useEffect, useState } from 'react';
+import { useDeviceContext } from './Context';
 function App() {
+  const device = useDeviceContext();
+  let gtd = '1fr 4fr';
+  let mx = 150;
+  function defections() {
+    if (device == 'mobile') return (gtd = '1fr'), (mx = '10');
+  }
+
+  console.log(device);
   return (
     <ThemeProvider colorMode="night">
       <BaseStyles>
@@ -13,7 +21,7 @@ function App() {
 
           <Nav />
 
-          <Grid gridTemplateColumns="1fr 4fr" mx={150}>
+          <Grid gridTemplateColumns={gtd} mx={mx}>
             <Bio />
 
             <Main />
--- src/Context.js
+++ src/Context.js
@@ -1,7 +1,7 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 
 export const DeviceContext = createContext();
-
+export const useDeviceContext = () => useContext(DeviceContext);
 function Context({ children }) {
   const [deviceState, setDeviceState] = useState('desktop');
   const getWidth = () =>
@@ -21,7 +21,7 @@ function Context({ children }) {
       window.removeEventListener('resize', resizeListener);
     };
   }, []);
-  console.log(deviceState);
+  //console.log(deviceState);
 
   return (
     <DeviceContext.Provider value={deviceState}>
