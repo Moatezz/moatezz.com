2be5eec7ee4a0350b16888c5b82c0670164a789a
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-18 10:29:36 +0100

    removed styles files

--- src/components/styles/WrapAvatar.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  width: min-content;
-  /* border: 1px solid white;
-  border-radius: 50%;
-  margin: 1rem; */
-`;
--- src/components/styles/WrapPadding.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  padding: ${(props) => props.padding || '1rem'};
-`;
